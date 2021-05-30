46751234b7fc04f6a2094fe23f07d3e63b83f664
ifeanyi23 <josephokorie32@gmail.com>
2021-05-30 18:04:13 +0100

    initial commit

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import AddCircleIcon from "@material-ui/icons/AddCircle";
+import Fab from "@material-ui/core/Fab";
+import Zoom from "@material-ui/core/Zoom";
+
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+
+  const [isTrue, setIsTrue] = useState(false);
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value
+      };
+    });
+  }
+  function handleCondition() {
+    setIsTrue(!isTrue);
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: ""
+    });
+    event.preventDefault();
+  }
+
+  return (
+    <div>
+      <form className="create-note">
+        {isTrue ? (
+          <input
+            name="title"
+            onChange={handleChange}
+            value={note.title}
+            placeholder="Title"
+          />
+        ) : null}
+
+        <textarea
+          name="content"
+          onChange={handleChange}
+          onClick={handleCondition}
+          value={note.content}
+          placeholder="Take a note..."
+          rows={isTrue ? 3 : 1}
+        />
+        <Zoom in={isTrue}>
+          <Fab onClick={submitNote}>
+            <AddCircleIcon />
+          </Fab>
+        </Zoom>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
