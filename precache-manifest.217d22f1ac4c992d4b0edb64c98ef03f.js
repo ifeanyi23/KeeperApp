f08409018e895ada39f773db6467f89d49cbccf3
ifeanyi23 <josephokorie32@gmail.com>
2021-05-31 01:26:00 +0100

    Updates

---
+++ precache-manifest.217d22f1ac4c992d4b0edb64c98ef03f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca5b71b01ab1f2c8f0031daf43881856",
+    "url": "/KeeperApp/index.html"
+  },
+  {
+    "revision": "e2cf02ea7deff2bc29a2",
+    "url": "/KeeperApp/static/js/2.62b14a52.chunk.js"
+  },
+  {
+    "revision": "b1b2d668794fcfaa5921",
+    "url": "/KeeperApp/static/js/main.444bfdf7.chunk.js"
+  },
+  {
+    "revision": "ad92294fb1599dc8ff6b",
+    "url": "/KeeperApp/static/js/runtime-main.ff26de38.js"
+  }
+]);
